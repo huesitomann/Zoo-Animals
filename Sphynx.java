c1df68ebe6daa268a59e5d4a295500da8e13a1f3
huesitomann <66976988+huesitomann@users.noreply.github.com>
2021-05-23 23:04:11 -0700

    Update Sphynx.java

--- Sphynx.java
+++ Sphynx.java
@@ -1,33 +1,34 @@
 package d;
 public class Sphynx implements IAnimal {
 	
-private int idTag = 0;
+	private int idTag = 0;
+	private String animalType = "Sphynx";
+	private final int MIN_TEMPERATURE = 36;
+	private final int MAX_TEMPERATURE = 115;
 
 
-public String getAnimalType() {
-	String animalType = "Sphynx";
-	return animalType;
-}
+
+	public String getAnimalType() {
+		return animalType;
+	}
 
 
-public int getIdTag() {
-	return idTag;
-}
+	public int getIdTag() {
+		return idTag;
+	}
 
 
-public void setIdTag(int idTag) {
-	this.idTag = idTag;
+	public void setIdTag(int idTag) {
+		this.idTag = idTag;
 	
-}
+	}
 
 
-public int getMinTemperature() {
-	int minTemperature = 36;
-	return minTemperature;
-}
+	public int getMinTemperature() {
+		return MIN_TEMPERATURE;
+	}
 
-public int getMaxTemperature() {
-	int maxTemperature = 115;
-	return maxTemperature;
-}
+	public int getMaxTemperature() {
+		return MAX_TEMPERATURE;
+	}
 }
