e6190f93851070b98d286c1409e1f05716e4f917
huesitomann <66976988+huesitomann@users.noreply.github.com>
2021-05-23 23:04:25 -0700

    Delete IAnimal.java

--- IAnimal.java
+++
@@ -1,9 +0,0 @@
-package d;
-
-public interface IAnimal {
-	public String getAnimalType();
-	public int getIdTag();
-	public void setIdTag(int anIdTag);
-	public int getMinTemperature();
-	public int getMaxTemperature();
-}
